5f329940c8e261b325790176791228f7e6d35a3d
Ryan Blue <ryanzblue@gmail.com>
2026-02-27 20:49:04 -0500

    Fix intake position again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -239,7 +239,7 @@ public final class Constants {
     public static final double reverseLimit = 0.0;
     public static final double forwardLimit = 0.25;
     public static final double homePosition = 0;
-    public static final double intakePosition = 0.26; // No kicker bar
+    public static final double intakePosition = 0.24; // No kicker bar
     public static final double stowPosition = 0;
   }
 
