4adb395703a23a5da76cf5eb250ac9cc6366bdf2
Ryan Blue <ryanzblue@gmail.com>
2026-03-03 16:20:21 -0500

    Add function to get hub center for alliance

--- src/main/java/frc/robot/FieldPoses.java
+++ src/main/java/frc/robot/FieldPoses.java
@@ -1,8 +1,15 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class FieldPoses {
   public static final Translation2d blueHubCenter = new Translation2d(4.620419, 4.034631);
   public static final Translation2d redHubCenter = new Translation2d(11.910219, 4.034631);
+
+  public static Translation2d getHubCenter() {
+    boolean isRed = DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red;
+    return isRed ? redHubCenter : blueHubCenter;
+  }
 }
