88cad47a9e911e12c2aa710e6d23cfdf3920d20e
Ryan Blue <ryanzblue@gmail.com>
2026-03-03 16:20:06 -0500

    Add shooter parameter calculator

---
+++ src/main/java/frc/ShooterParameterCalculator.java
@@ -0,0 +1,25 @@
+package frc;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
+
+public class ShooterParameterCalculator {
+
+    public static InterpolatingTreeMap<Double, ShooterParameters> map = new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), ShooterParameters::interpolate);
+
+    static {
+        map.put(0.5, new ShooterParameters(0, 42.0)); // hub shot
+    }
+    public record ShooterParameters(double hoodPosition, double shooterVelocity) {
+        public static ShooterParameters interpolate(ShooterParameters start, ShooterParameters end, double t) {
+            return new ShooterParameters(
+                    MathUtil.interpolate(start.hoodPosition, end.hoodPosition, t),
+                    MathUtil.interpolate(start.shooterVelocity, end.shooterVelocity, t));
+        }
+    }
+
+    public static ShooterParameters calculate(double distance) {
+        return map.get(distance);
+    }
+}
